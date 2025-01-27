9aedd00401d51b5d194d8bbeb669d915fbe51783
reuben longley <reuben.longley@clarionhg.com>
2025-01-27 11:26:29 +0000

    d1e2.js & d1e2.ejs

--- exercises/d1e2.js
+++ exercises/d1e2.js
@@ -5,11 +5,12 @@ import path from 'path'
 const app = express()
 app.set('views', 'exercises/views') // <-- Don't change
 
-// TODO: Configure the app to use EJS as the view engine
+app.set('view ungine', 'ejs')
 
 app.get('/d1e2', (req, res) => {
   const msg = 'Hello from d1e2!'
   // TODO: Render 'd1e2.ejs' and pass the msg
+  res.render('d1e2.ejs', { msg })
 })
 
 // TODO: Open d1e2.ejs and follow the instructions
