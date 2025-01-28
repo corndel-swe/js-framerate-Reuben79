7e736f0fbc04900cf5725654a6d978fce4dacf90
reuben longley <reuben.longley@clarionhg.com>
2025-01-28 12:11:33 +0000

    WS1

--- server/app.js
+++ server/app.js
@@ -6,14 +6,9 @@ const app = express()
 app.use(express.static('exercises/public'))
 
 app.get("/", async(req, res)=> {
-    try {
     const results = await Movie.findAll()
     res.send(results)
-    } catch (error) {
-    console.error(error)
-    res.status(500). send('An error occurred')
-    }
-
+   
 })
 
 export default app
